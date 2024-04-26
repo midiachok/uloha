f24942eec4a0d8e9ea72a879b688c3ed6bc83b67
midiachok <162707182+midiachok@users.noreply.github.com>
2024-04-26 13:40:44 +0200

    zmena strategie

--- uloha.c
+++ uloha.c
@@ -2,39 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-char nahodny_polynom(float *q,char rad,char k){
+char nahodny_polynom(float *q,short rad,short k){
 
-if(k>rad)
+if(k>rad){
 return 0;
-
-do{
-
-
+}
 
 //generujeme nejake k korenov
 float* korene = calloc(k, sizeof(float));
 for(int j=0;j<k;j++){
-korene[j]=rand()%21-10;
+korene[j]=rand()%21-10; //ale integer!
 }
 
+/*funkcia na nasobenie polynomov*/
+
 /*generujeme koeficienty rovnice podla vygenerovanych korenov
-tu sa pouzije Vietov vztah ale ako?? a co s tymi nasobnostami!!
+REZIDUUM
 */
 
-for(int i=rad;i>=0;i--){
-        q[i]=0; 
 
-       
-        for(int j=0;j<=k;j++){
-            
-            if(i!=j){
-                if(q[i]==0){
-                    q[i]=1; 
-                }
-                q[i]*=-korene[j];
-            }
-        }
-    }
 
 /*overujeme dve podmienky ktore zistia ci ma uloha riesenie (vtedy oznamime kod (ne)uspechu
 spravne rozumiem ze 'uloha ma riesenie' nehovori o korenoch teda nemusi kod hovorit ci mame 
@@ -44,50 +30,37 @@ kv.rov. x^2-3x+10=0, korene ziadne nemame ale neznamena to ze mame neuspech. zna
 mame k=0,rad=2,q[]={1,-3,10} a musime oznamit kod uspechu. 
 to znamena ze musime nejakym padom overovat ci vygenerovane korene sa hodia na vytvorenie 
 aspon nejakych koeficientov. ak ano tak mame kod uspechu.
-mozne podmienky na overovanie: 1. ci k<=rad  2. ci urcite polynom ma k korenov (ak hodnota polynomu
-sa = 0, tak je koren, a k vtedy ++)
+
+
+podmienky na overovanie: 1. k<=rad  2. ak rad je parny tak k tiez parny a naopak
 */
 
-float hodnota_polynomu(float *q,char rad,float x) {
-    float hodnota=q[0];
-    for(char i=1;i<=rad;i++){
-        hodnota=hodnota*x+q[i];
-    }
-    return hodnota;
-}
 
-char pocet_korenov = 0;
-    for (float x = -10.0; x <= 10.0; x += 0.01) {
-        if (hodnota_polynomu(q, rad, x) == 0.0) {
-            pocet_korenov++;
-        }
-    }
 
-}while(pocet_korenov!=k);
-return 1;
+
 /*ak kod uspechu tak vypisat polynom
 ak neuspechu tak napisat ze polynom neexistuje
 */
 
-/*a toto vsetko zatial bez algebraickych nasobnosti!!!*/
+
 }
 
 
 
 int main(){
 srand(time(0));
-char rad=0,k=0;
+short rad=0,k=0;
 
 
 printf("Zadajte stupen polynomu: ");
-    scanf("%c", &rad);
+    scanf("%hd", &rad);
     printf("Zadajte pocet korenov: ");
-    scanf(" %c", &k);
+    scanf(" %hd", &k);
 float* q = calloc(rad + 1, sizeof(float));
     
     if (nahodny_polynom(q,rad,k)){
     for (int i=0;i<=rad;i++){
-            printf("%fx^%c",q[i],rad-i);
+            printf("%fx^%hd",q[i],rad-i);
         }
         printf("=0\n");
     }else{
